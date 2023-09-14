557fce2f849d0e4f8869032e87943493559d8537
Massvdev <107349168+Massvdev@users.noreply.github.com>
2023-09-13 22:43:03 -0500

    Update [slug].png.ts

--- src/pages/v1/generate/og/[slug].png.ts
+++ src/pages/v1/generate/og/[slug].png.ts
@@ -26,17 +26,15 @@ export const GET: APIRoute = async ({ params, props }) => {
   const description = props.description ?? null;
   const html = toReactElement(`
   <div style="background-color: white; display: flex; flex-direction: column; height: 100%; padding: 3rem; width: 100%">
-    <div style="display:flex; height: 100%; width: 100%; background-color: white; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
+    <div style="display:flex; height: 100%; width: 100%; background-color: teal; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
       <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; filter: drop-shadow()">
-        <div style="display: flex; justify-content: space-between;">
-          <div style="display: flex; flex-direction: column; gap: 0.75rem;">
-            <p style="font-size: 48px;">Brutal theme for Astro</p>
-            <p style="font-size: 38px;">${title}</p>
+        <div style="display: flex; flex-direction: column; gap: 0.75rem;">  
+          <p style="font-size: 48px;">NetaDAØ</p>
+          <p style="font-size: 38px;">A cosmos community collective</p>
           </div>
-          <img src="https://www.elian.codes/assets/img/elian.jpg" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
-        </div>
-        <div style="display: flex;">
-          <p style="font-size: 24px;">${description}</p>
+        <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
+          <p style="font-size: 32px">${link}</p>
+          <img src="https://neta-astro.vercel.app/assets/img/netaltx.png" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
         </div>
       </div>
     </div>
